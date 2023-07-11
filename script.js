2be075d46dfed2dc66c4b8614923cc8988b65cdf
StanleyRasp <stas@vdb.com.pl>
2023-07-11 16:32:08 +0200

    style the categories bar

---
+++ script.js
@@ -0,0 +1,19 @@
+const categoriesList = document.querySelector(".categories-list")
+const categoryListWrapper = document.querySelector(".category-list-wrapper")
+
+categoriesList.addEventListener("scroll", event => {
+    if (categoriesList.scrollLeft + categoriesList.offsetWidth === categoriesList.scrollWidth) {
+        categoryListWrapper.classList.remove("show-fade")
+        categoryListWrapper.classList.add("hide-fade")
+        setTimeout(() => {
+            categoryListWrapper.classList.add("disable-after")
+        }, 500);
+    } else{
+        if (categoryListWrapper.classList.contains("disable-after")){
+            categoryListWrapper.classList.remove("disable-after")
+            categoryListWrapper.classList.add("show-fade")
+            categoryListWrapper.classList.remove("hide-fade")
+        }
+    }
+    
+})
